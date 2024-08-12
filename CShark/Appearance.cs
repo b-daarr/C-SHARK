59622efd429af1efbcd85c5023ba0ceab48f877e
b̴⊶d̴aar <118155606+b-daarr@users.noreply.github.com>
2024-08-12 09:57:55 +0330

    V-2024.08.12

---
+++ CShark/Appearance.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CShark
+{
+    public partial class Appearance : Form
+    {
+        public Appearance()
+        {
+            InitializeComponent();
+        }
+
+        private static string rootfile = @"D:\\cshark_cache.json";
+        private string json = File.ReadAllText(rootfile);
+
+        public void LightMode()
+        {
+            var objects = JObject.Parse(json);
+            objects["app_theme"] = "light";
+            File.WriteAllText(rootfile, objects.ToString(Formatting.Indented));
+
+            BackColor = Color.FromArgb(0, 100, 255);
+            logo.BackColor = Color.FromArgb(0, 100, 255);
+            name.BackColor = Color.FromArgb(0, 100, 255);
+            switch_mode.BackColor = Color.FromArgb(0, 100, 255);
+            mode_pic.BackColor = Color.FromArgb(0, 100, 255);
+            mode_status.BackColor = Color.FromArgb(0, 100, 255);
+            back_button.BackColor = Color.FromArgb(0, 100, 255);
+            status.BackColor = Color.FromArgb(0, 100, 255);
+            mode_status.ForeColor = Color.Black;
+            status.ForeColor = Color.Black;
+            switch_mode.BackgroundImage = Resources.disable;
+            mode_pic.BackgroundImage = Resources.lightmode;
+            mode_status.Text = "Day Mode";
+        }
+
+        public void DarkMode()
+        {
+            var objects = JObject.Parse(json);
+            objects["app_theme"] = "dark";
+            File.WriteAllText(rootfile, objects.ToString(Formatting.Indented));
+
+            BackColor = Color.FromArgb(64, 0, 64);
+            logo.BackColor = Color.FromArgb(64, 0, 64);
+            name.BackColor = Color.FromArgb(64, 0, 64);
+            switch_mode.BackColor = Color.FromArgb(64, 0, 64);
+            mode_pic.BackColor = Color.FromArgb(64, 0, 64);
+            mode_status.BackColor = Color.FromArgb(64, 0, 64);
+            back_button.BackColor = Color.FromArgb(64, 0, 64);
+            status.BackColor = Color.FromArgb(64, 0, 64);
+            mode_status.ForeColor = Color.FromArgb(255, 192, 255);
+            status.ForeColor = Color.White;
+            switch_mode.BackgroundImage = Resources.enable;
+            mode_pic.BackgroundImage = Resources.darkmode;
+            mode_status.Text = "Night Mode";
+        }
+
+        private void switch_mode_Click(object sender, EventArgs e)
+        {
+            var objects = JObject.Parse(json);
+            Main main = new Main();
+
+            if (objects["app_theme"].ToString() == "dark")
+            {
+                LightMode();
+            }
+            else if (objects["app_theme"].ToString() == "light")
+            {
+                DarkMode();
+            }
+        }
+
+        private void back_button_MouseEnter(object sender, EventArgs e)
+        {
+            status.Visible = true;
+        }
+
+        private void back_button_MouseLeave(object sender, EventArgs e)
+        {
+            status.Visible = false;
+        }
+
+        private void back_button_Click(object sender, EventArgs e)
+        {
+            Main main = new Main();
+            main.Show();
+            this.Close();
+        }
+
+        private void Appearance_Load(object sender, EventArgs e)
+        {
+            Main main = new Main();
+            main.RootFileTest();
+
+            var objects = JObject.Parse(json);
+
+            if (objects["app_theme"].ToString() == "light")
+            {
+                LightMode();
+            }
+            else if (objects["app_theme"].ToString() == "dark")
+            {
+                DarkMode();
+            }
+        }
+    }
+}
---
+++ CShark/Config.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CShark
+{
+    public partial class Config : Form
+    {
+        public Config()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(Config_KeyDown);
+            this.KeyPreview = true;
+        }
+
+        private void back_button_MouseEnter(object sender, EventArgs e)
+        {
+            status.Visible = true;
+        }
+
+        private void back_button_MouseLeave(object sender, EventArgs e)
+        {
+            status.Visible = false;
+        }
+
+        private void back_button_Click(object sender, EventArgs e)
+        {
+            Main main = new Main();
+            main.Show();
+            this.Close();
+        }
+
+        private static string rootfile = @"D:\\cshark_cache.json";
+        private string json = File.ReadAllText(rootfile);
+
+        public void LightMode()
+        {
+            BackColor = Color.FromArgb(0, 100, 255);
+            logo.BackColor = Color.FromArgb(0, 100, 255);
+            name.BackColor = Color.FromArgb(0, 100, 255);
+            back_button.BackColor = Color.FromArgb(0, 100, 255);
+            remove.BackColor = Color.FromArgb(0, 100, 255);
+            export.BackColor = Color.FromArgb(0, 100, 255);
+            import.BackColor = Color.FromArgb(0, 100, 255);
+            config_list.BackColor = Color.FromArgb(0, 100, 255);
+            status.ForeColor = Color.Black;
+            name.ForeColor = Color.Black;
+            item_status.ForeColor = Color.Black;
+        }
+
+        public void DarkMode()
+        {
+            BackColor = Color.FromArgb(64, 0, 64);
+            logo.BackColor = Color.FromArgb(64, 0, 64);
+            name.BackColor = Color.FromArgb(64, 0, 64);
+            back_button.BackColor = Color.FromArgb(64, 0, 64);
+            remove.BackColor = Color.FromArgb(64, 0, 64);
+            export.BackColor = Color.FromArgb(64, 0, 64);
+            import.BackColor = Color.FromArgb(64, 0, 64);
+            config_list.BackColor = Color.FromArgb(64, 0, 64);
+            status.ForeColor = Color.White;
+            name.ForeColor = Color.White;
+            item_status.ForeColor = Color.White;
+        }
+
+        private void Config_Load(object sender, EventArgs e)
+        {
+            Main main = new Main();
+            main.RootFileTest();
+
+
+            var jsonObject = JObject.Parse(json);
+
+            if (jsonObject["app_theme"].ToString() == "light")
+            {
+                LightMode();
+            }
+            else if (jsonObject["app_theme"].ToString() == "dark")
+            {
+                DarkMode();
+            }
+        }
+
+        private void import_MouseEnter(object sender, EventArgs e)
+        {
+            item_status.Text = "Import Config";
+            hint.SetToolTip(import, "Ctrl + I");
+        }
+
+        private void import_MouseLeave(object sender, EventArgs e)
+        {
+            item_status.Text = string.Empty;
+        }
+
+        private void export_MouseEnter(object sender, EventArgs e)
+        {
+            item_status.Text = "Export Config";
+            hint.SetToolTip(export, "Ctrl + E");
+        }
+
+        private void export_MouseLeave(object sender, EventArgs e)
+        {
+            item_status.Text = string.Empty;
+        }
+
+        private void remove_MouseEnter(object sender, EventArgs e)
+        {
+            item_status.Text = "Remove Config";
+            hint.SetToolTip(remove, "Ctrl + R");
+        }
+
+        private void remove_MouseLeave(object sender, EventArgs e)
+        {
+            item_status.Text = string.Empty;
+        }
+
+        private void Config_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.I)
+            {
+                //
+            }
+            else if (export.Enabled == true && e.Control && e.KeyCode == Keys.E)
+            {
+                //
+            }
+            else if (remove.Enabled == true && e.Control && e.KeyCode == Keys.R)
+            {
+                //
+            }
+        }
+    }
+}
